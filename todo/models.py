5d14c001b8f21a2deb2c885418e09715621ef79f
YJKBigBo <yjg0557@naver.com>
2023-06-02 20:07:16 +0900

    수정과 삭제기능 만들기

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model): #장고에서 선언해 놓은 모델스를 사용하기 위해 클래스 사용
+	todo = models.CharField(max_length=30)
+	description = models.TextField()
+	important = models.BooleanField(default=False)
+	complete = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f'{self.todo} [{self.id}]'
--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-
-from todo.views import todos
+from todo.views import create_todo, delete_todo, index, todos, update_todo
 
 urlpatterns = [
-	path('', todos),
-    
+	path('', todos, name="todos"),
+	path('index/', index),
+	path('create_todo/', create_todo, name="create"),
+	path('update_todo/<int:pk>/', update_todo, name='update'),
+	path('delete_todo/<int:pk>/', delete_todo, name='delete'),
 ]
